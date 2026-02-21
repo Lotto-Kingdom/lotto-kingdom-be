abb17581f37b35e786aaff47fac6a028f82a9e32
pyjhoop <pyjhoop@naver.com>
2026-02-21 14:39:58 +0900

    init: 로또나라 스프링 프로젝트 생성

---
+++ src/test/java/com/example/lottokingdombe/LottoKingdomBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lottokingdombe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoKingdomBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
